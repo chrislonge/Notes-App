5e1853a85e951e2f6a503c31201b72220929ff78
Chris Longe <chris.longe@capitalone.com>
2018-01-05 14:07:18 -0500

    Initial commit including .gitingore file.

---
+++ Notes-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes-App
+//
+//  Created by Longe, Chris on 1/5/18.
+//  Copyright © 2018 Capital One. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
